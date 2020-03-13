9eac14b4c5456c4be24740f472a763ae7a730e41
Rodrigo Nazar <rnazar@uc.cl>
2020-03-12 23:40:54 -0300

    Playing with opencv

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # T1-Patrones
 Repositorio para tarea 1 Patrones
+
+
+## Comandos útiles
+
+* Revisar https://www.youtube.com/watch?v=CdltAssTMs8&list=PLS1QulWo1RIa7D1O6skqDQ-JZ1GGHKK-K&index=15
--- apps/cv2_tests.py
+++ apps/cv2_tests.py
@@ -1,12 +1,42 @@
 import cv2 as cv
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def printImg(img, factor):
+    if len(img.shape) == 2:
+        x, y = img.shape
+    elif len(img.shape) == 3:
+        x, y, _ = img.shape
+
+    x = int(x/factor)
+    y = int(y/factor)
+
+    imgr = cv.resize(img, (y, x))
+
+    cv.imshow('Training_01', imgr)
+    cv.waitKey(3000)
+    cv.destroyAllWindows()
 
 
 def main():
     img = cv.imread('pics/Training_01.png')
-    img = cv.resize(img, (960, 540))
-    cv.imshow('Training_01', img)
-    cv.waitKey(0)
-    # cv.destroyAllWindows()
+
+    # img = cv.rectangle(img, (500, 500), (1000, 1000), (255, 0, 0), 50)
+
+    img_R, img_G, img_B = cv.split(img)
+
+    # _, th_R = cv.threshold(img_R, 127, 255, cv.THRESH_BINARY)
+
+    print(img_B.equal)
+
+    # printImg(th_R, 4)
+
+    # hist, bins = np.histogram(img_R, bins=3)
+    # plt.hist(th_R)
+    # plt.show()
+
+    # printImg(img, 4)
 
 
 if __name__ == '__main__':
