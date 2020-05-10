6406f24172b47bebbc7b671b085145edc5cad2a3
Rodrigo Nazar <rnazar@uc.cl>
2020-05-10 01:13:58 -0400

    fishering

--- Tarea 2/apps/feature_selection.py
+++ Tarea 2/apps/feature_selection.py
@@ -5,51 +5,37 @@ mod = '''
 import numpy as np
 
 
-def centerOfMass(data):
-    N, M = data.shape
-    return np.sum(data, axis = 0)/N
-
-
-
-def jFisher(data, labels, n_features):
+def jFisher(data, labels):
     '''
     Retorna los índices de las características elegidas
     '''
-    N, M = data.shape
+    if len(data.shape) == 2:
+        N, M = data.shape
+    elif len(data.shape) == 1:
+        N, M = data.shape[0], 1
+    else:
+        N, M = data.shape[0], data.shape[1]
 
     # Centro de masa de todas las muestras
-    barZ = centerOfMass(data)
+    barZ = data.mean(axis=0)
 
-    # Para calular el centro de masa por clase
-    classes = list(set(i for i in labels))
-    indx = {i: [] for i in classes}
-    for _class in classes:
-        indx[_class] = np.argwhere(labels == _class)
+    classes = list(set(i for i in labels)) # Las distintas clases
 
-    # Centro de masa por clase
-    barZk = {_class: centerOfMass(data[indx[_class][:, 0]]) for _class in classes}
-
-
-    # Para calcular la matriz de covarianza interclase
+    # Indices de cada elemento de cada clase
+    indx = {_class: np.argwhere(labels == _class) for _class in classes}
+    # Centro de masa por clase:
+    barZk = {_class: data[indx[_class][:, 0]].mean(axis=0) for _class in classes}
+    # Probabilidad de ocurrencia de cada clase
     pk = {_class: indx[_class].shape[0]/N for _class in classes}
 
-    # Matriz de covarianza interclase
-    Cb = np.zeros((M, M))
+    # Matrices de covarianza inter e intra clases
+    Cw, Cb = np.zeros((M, M)), np.zeros((M, M))
+
     for _class in classes:
-        elem = barZk[_class] - barZ
-        Cb += pk[_class]*elem.dot(np.transpose(elem))
+        Cb_diff = (barZk[_class] - barZ).reshape((M, 1))
+        Cb += pk[_class] * np.dot(Cb_diff, Cb_diff.T)
+        print('Cb', Cb.shape)
 
-
-    # Para calcular la matriz de covarianza intraclase
-    Cw = np.zeros((M, M))
-    for _class in classes:
-        Ck = np.zeros((M, M))
-        for sample in data[indx[_class][:, 0]]: # Obtengo los elementos de la clase
-            elem = sample - barZk[_class]
-            Ck += elem.dot(np.transpose(elem))
-        Ck /= data[indx[_class][:, 0]].shape[0] - 1
-
-        Cw += pk[_class]*Ck
 
     try:
         return np.trace(np.linalg.inv(Cw).dot(Cb))
@@ -62,13 +48,22 @@ def sfs(data, labels, n_features):
     N, M = data.shape
 
     selected_features = []
-    remaining_feats = [i for i in range(M)]
-
-    print(N, M)
-
+    remaining_features = [i for i in range(M)]
+    current_scores = {}
 
     for _ in range(n_features):
-        pass
+        for inx, feature in enumerate(remaining_features):
+            features = selected_features + [feature]
+            print(features)
+            # print(data[:, features].shape)
+            current_scores[jFisher(data[:, features], labels)] = inx
+
+        best = current_scores[max(current_scores.keys())]
+        print(best, max(current_scores.keys()))
+
+        selected_features.append(best)
+        remaining_features.remove(best)
+        current_scores = {}
 
     return np.array(selected_features)
 
